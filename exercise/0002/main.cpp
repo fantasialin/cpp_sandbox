d9dbd63a39c6abf01fbc298b488d63083db0d81c
fantasia <fantasialin@gmail.com>
2022-04-12 16:01:28 +0800

    fix compiling error on clang++.

--- exercise/0002/main.cpp
+++ exercise/0002/main.cpp
@@ -40,15 +40,20 @@ class printer
 int main(int argc, char **argv){
     cout << "exercise - 0002" << endl;
     printer p;
+    string j1 = string("This is first job");
+    string j2 = string("This is second job");
+    string j3 = string("This is third job");
+    string j4 = string("This is fourth job");
 
-    p.addjob(string("This is first job"));
-    p.addjob(string("This is second job"));
-    p.addjob(string("This is third job"));
+
+    p.addjob(j1);
+    p.addjob(j2);
+    p.addjob(j3);
 
 
     p.dump_job_queue();
 
-    p.add_to_top(string("This is fourth job"));
+    p.add_to_top(j4);
     
     p.dump_job_queue();
 
